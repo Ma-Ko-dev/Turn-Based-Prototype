f2b984f4b04339a88d3c4c45301090e947fc8073
Mathias K. <Mathias.Kowitz@gmx.de>
2026-02-07 16:09:04 +0100

    feat: automated size modifiers for AC and attack

--- scripts/UnitData.gd
+++ scripts/UnitData.gd
@@ -7,8 +7,10 @@ class_name UnitData
 @export var is_player_data: bool = false
 
 # --- Identity ---
+enum Size { FINE, DIMINUTIVE, TINY, SMALL, MEDIUM, LARGE, HUGE, GARGANTUAN, COLOSSAL }
 @export_group("Identity")
 @export var name: String = "Unknown Unit"
+@export var size: Size = Size.MEDIUM
 @export var texture: Texture2D
 
 # --- Stats ---
@@ -40,16 +42,28 @@ var extra_initiative_bonus: int = 0
 @export_group("AC Bonus")
 @export var armor_bonus: int = 0 # from actual armor
 @export var shield_bonus: int = 0
-@export var size_bonus: int = 0 # +1 for Small, -1 for Large, 0 Medium
 
 
 ## Helper: Pathfinder modifier calculation
 func get_modifier(score: int) -> int:
 	return floor((score - 10) /  2.0)
 
+func get_size_modifier() -> int:
+	match size:
+		Size.FINE: return 8
+		Size.DIMINUTIVE: return 4
+		Size.TINY: return 2
+		Size.SMALL: return 1
+		Size.LARGE: return -1
+		Size.HUGE: return -2
+		Size.GARGANTUAN: return -4
+		Size.COLOSSAL: return -8
+		_: return 0 # Medium
+
 ## The "True" AC of this unit type
 func get_armor_class() -> int:
-	return 10 + get_modifier(dexterity) + armor_bonus + shield_bonus + size_bonus
+	# Size bonus to AC is the same as the size bonus to Attack
+	return 10 + get_modifier(dexterity) + armor_bonus + shield_bonus + get_size_modifier()
 
 ## Calculates initial HP. Level 1 players get max roll.
 func calculate_initial_hp() -> int:
@@ -66,9 +80,9 @@ func get_initiative_bonus() -> int:
 
 ## Get total attack bonus
 func get_attack_bonus() -> int:
-	return base_attack_bonus + get_modifier(strength)
+	return base_attack_bonus + get_modifier(strength) + get_size_modifier()
 
 ## Total Saving Throws
 func get_fort_save() -> int: return base_fortitude + get_modifier(constitution)
 func get_reflex_save() -> int: return base_reflex + get_modifier(dexterity)
-func get_will_save() -> int: return base_will + get_modifier(intelligence)
+func get_will_save() -> int: return base_will + get_modifier(wisdom)
