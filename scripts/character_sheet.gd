d92e16f4c5d31d2e5516a0f214e58d6a30009a6a
Mathias K. <Mathias.Kowitz@gmx.de>
2026-02-10 15:38:49 +0100

    fix: hide UI on ready and handle Tab focus

--- scripts/character_sheet.gd
+++ scripts/character_sheet.gd
@@ -7,11 +7,13 @@ extends Control
 
 
 func _ready() -> void:
+	# Hide on gamestart
+	self.hide()
 	# Connect the signal and show first tab
 	tabs.tab_changed.connect(_on_tab_changed)
 	_update_view(0)
 	if close_button:
-		close_button.pressed.connect(self.hide)
+		close_button.pressed.connect(_on_close_pressed)
 
 func _on_tab_changed(tab_index: int) -> void:
 	_update_view(tab_index)
@@ -21,3 +23,7 @@ func _update_view(index: int) -> void:
 	# toggle visibility based on selected tab index
 	for i in range(content_area.get_child_count()):
 		content_area.get_child(i).visible = (i == index)
+
+
+func _on_close_pressed() -> void:
+	self.hide()
