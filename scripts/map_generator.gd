f9dacc3df05fcf19e73d763f14d05149e74c710c
Mathias K. <Mathias.Kowitz@gmx.de>
2026-02-25 10:56:12 +0100

    refactor: path gen to connect map sides
    refactor: removed magic numbers

--- scripts/map_generator.gd
+++ scripts/map_generator.gd
@@ -1,29 +1,46 @@
+@tool
 extends Node2D
 
+@export_group("Map Dimensions")
 @export var map_width: int = 50
 @export var map_height: int = 50
+@export_group("Tile Settings")
+@export var tile_ground: Vector2i = Vector2i(8,5)
+@export var tiles_path: Array[Vector2i] = [Vector2i(1,0), Vector2i(2,0), Vector2i(3,0), Vector2i(4,0)]
+@export var tiles_obstacles: Array[Vector2i] = [
+	Vector2i(5,0), Vector2i(6,0), Vector2i(7,0),
+	Vector2i(0,1), Vector2i(1,1), Vector2i(2,1), Vector2i(3,1), Vector2i(4,1), Vector2i(5,1),
+	Vector2i(0,2), Vector2i(1,2), Vector2i(2,2), Vector2i(3,2), Vector2i(4,2), 
+]
+@export_tool_button("Generate Map") var map_gen_button = generate_full_map
+
 
 @onready var ground_layer: TileMapLayer = $GroundLayer
 @onready var decoration_layer: TileMapLayer = $DecorationLayer
 @onready var obstacle_layer: TileMapLayer = $ObstacleLayer
 
 
-func _input(event: InputEvent) -> void:
-	if event is InputEventKey and event.pressed and event.keycode == KEY_F5:
-		generate_full_map()
-
+#func _ready() -> void:
+	#generate_full_map()
 
 func generate_full_map() -> void:
+	_clear_layers()
+	_fill_ground()
+	_generate_organic_paths()
+	_populate_objects()
+	_sync_systems()
+
+func _clear_layers() -> void:
 	ground_layer.clear()
 	decoration_layer.clear()
 	obstacle_layer.clear()
-	
+
+func _fill_ground() -> void:
 	for x in range(map_width):
 		for y in range(map_height):
-			# tile coords 8,5
-			ground_layer.set_cell(Vector2i(x, y), 0, Vector2i(8, 5))
-	_generate_organic_paths()
-	_populate_objects()
+			ground_layer.set_cell(Vector2i(x, y), 0, tile_ground)
+
+func _sync_systems() -> void:
 	var map_manager = get_tree().get_first_node_in_group("map_manager")
 	if map_manager and map_manager.has_method("update_astar_grid"):
 		map_manager.update_astar_grid()
@@ -31,16 +48,31 @@ func generate_full_map() -> void:
 	if player and player.has_method("update_camera_limits"):
 		player.update_camera_limits()
 
-
 func _generate_organic_paths() -> void:
-	var path_tiles = [Vector2i(1,0), Vector2i(2,0), Vector2i(3,0), Vector2i(4,0)]
-	var curr = Vector2i(map_width / 2, map_width / 2)
-	for i in range(400):
-		decoration_layer.set_cell(curr, 0, path_tiles.pick_random())
-		curr += [Vector2i.UP, Vector2i.DOWN, Vector2i.LEFT, Vector2i.RIGHT].pick_random()
-		curr.x = clamp(curr.x, 0, map_width - 1)
-		curr.y = clamp(curr.y, 0, map_height - 1)
+	var h_start = Vector2i(0, randi_range(5, map_height - 5))
+	var h_end = Vector2i(map_width - 1, randi_range(5, map_height - 5))
+	_create_path_connection(h_start, h_end)
+	var v_start = Vector2i(randi_range(5, map_width - 5), 0)
+	var v_end = Vector2i(randi_range(5, map_width - 5), map_height - 1)
+	_create_path_connection(v_start, v_end)
 
+func _create_path_connection(start: Vector2i, end: Vector2i) -> void:
+	if tiles_path.is_empty():
+		push_error("MapGenerator: tiles_path is empty! Check Inspector.")
+		return
+	var curr = start
+	var steps = 0
+	var max_steps = map_width * map_height 
+	while curr != end and steps < max_steps:
+		steps += 1
+		decoration_layer.set_cell(curr, 0, tiles_path.pick_random())
+		if randf() < 0.5 and curr.x != end.x:
+			curr.x += 1 if end.x > curr.x else -1
+		elif curr.y != end.y:
+			curr.y += 1 if end.y > curr.y else -1
+		decoration_layer.set_cell(curr, 0, tiles_path.pick_random())
+		if curr.y + 1 < map_height:
+			decoration_layer.set_cell(curr + Vector2i.DOWN, 0, tiles_path.pick_random())
 
 func _populate_objects() -> void:
 	for x in range(map_width):
@@ -48,5 +80,5 @@ func _populate_objects() -> void:
 			var coords = Vector2i(x, y)
 			if decoration_layer.get_cell_source_id(coords) != -1: continue # keep paths clear
 			var roll = randf()
-			if roll < 0.1:
-				obstacle_layer.set_cell(coords, 0, [Vector2i(5,0), Vector2i(6,0), Vector2i(7,0), Vector2i(0,1), Vector2i(1,1), Vector2i(2,1)].pick_random())
+			if roll < 0.15: # 15% trees/hard terrain
+				obstacle_layer.set_cell(coords, 0, tiles_obstacles.pick_random())
